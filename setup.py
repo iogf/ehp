adb4bbd49ab7faa6c0cc99d339a104e09fba6abd
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2017-06-24 11:21:47 -0300

    Fixing setup.py version.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-#! /usr/bin/env python
-
 from distutils.core import setup
+
 setup(name="ehp",
-      version="1.0.0",
+      version="1.1.0",
       py_modules=["ehp"],
       author="Iury O. G. Figueiredo",
       author_email="ioliveira@id.uff.br",
       url="",
       Description="Parsing html/xml documents in a faster and easier way")
+
 
 
 
